5383524b1737d186d06321571d7764607a8e6597
tishenko-va <valeriaalistarhova778@gmail.com>
2024-07-04 22:22:25 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+homework=12
+hours=1.5
+name ="Pyton"
+print(hours / homework)   #0.125
+time_for_task=0.125
+print("Курс:",name,",всего задач:",homework,",затрачено часов:",hours,",время выполнения",time_for_task,)
+
